32e889976a1c393cde1518cf8da431c5484efaa5
liqiang <liqiang80425@163.com>
2024-02-12 09:35:44 +0800

    ProductMonitor

--- ProductMonitor/ProductMonitor/ViewModels/MainWindowVM.cs
+++ ProductMonitor/ProductMonitor/ViewModels/MainWindowVM.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -23,7 +24,8 @@ namespace ProductMonitor.ViewModels
         /// </summary>
         public UserControl MonitorUC
         {
-            get {
+            get
+            {
                 if (_MonitorUC == null)
                 {
                     _MonitorUC = new MonitorUC();
@@ -40,6 +42,41 @@ namespace ProductMonitor.ViewModels
             }
         }
 
+        /// <summary>
+        /// 时间 小时:分钟
+        /// </summary>
+        public string TimeStr
+        {
+            get
+            {
+                return DateTime.Now.ToString("HH:mm");
+            }
+        }
 
+        /// <summary>
+        /// 日期 年-月-日
+        /// </summary>
+        public string DateStr
+        {
+            get
+            {
+                return DateTime.Now.ToString("yyyy-MM-dd");
+            }
+        }
+
+        /// <summary>
+        /// 星期
+        /// </summary>
+        public string WeekStr
+        {
+            get
+            {
+                int index = (int)DateTime.Now.DayOfWeek;
+
+                string[] week = new string[7] { "星期日", "星期一" , "星期二" , "星期三" ,"星期四" ,"星期五" ,"星期六" };
+                
+                return week[index];
+            }
+        }
     }
 }
