d956af49c53dcba6c3c3dbcffc87b187c061a018
liqiang <liqiang80425@163.com>
2024-02-14 11:22:34 +0800

    ProductMonitor

--- ProductMonitor/ProductMonitor/obj/Debug/net6.0-windows/App.g.i.cs
+++ ProductMonitor/ProductMonitor/obj/Debug/net6.0-windows/App.g.i.cs
@@ -59,7 +59,7 @@ namespace ProductMonitor {
             
             #line default
             #line hidden
-            System.Uri resourceLocater = new System.Uri("/ProductMonitor;V1.0.0.0;component/app.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/ProductMonitor;component/app.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\App.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
