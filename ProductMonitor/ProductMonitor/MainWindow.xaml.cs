880e0743ecc2631cd157e7cf8af0186d99fac648
liqiang <liqiang80425@163.com>
2024-02-17 17:49:04 +0800

    ProductMonitor

--- ProductMonitor/ProductMonitor/MainWindow.xaml.cs
+++ ProductMonitor/ProductMonitor/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using ProductMonitor.ViewModels;
+﻿using ProductMonitor.OpCommand;
+using ProductMonitor.UserControls;
+using ProductMonitor.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,6 +12,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -21,11 +24,75 @@ namespace ProductMonitor
     /// </summary>
     public partial class MainWindow : Window
     {
+        /// <summary>
+        /// 视图模型
+        /// </summary>
+        MainWindowVM mainWindowVM = new MainWindowVM();
+
         public MainWindow()
         {
             InitializeComponent();
-            MainWindowVM mainWindowVM = new MainWindowVM();
             this.DataContext = mainWindowVM;
+        }
+
+        /// <summary>
+        /// 显示车间详情页
+        /// </summary>
+        private void ShowDetailUC()
+        {
+            WorkShopDetailUC workShopDetailUC = new WorkShopDetailUC();
+
+            mainWindowVM.MonitorUC = workShopDetailUC;
+
+            //动画效果(由下而上)
+            //位移 移动时间
+            ThicknessAnimation thicknessAnimation = new ThicknessAnimation(new Thickness(0,50,0,-50),new Thickness(0,0,0,0),new TimeSpan(0,0,0,0,400));
+
+            //透明度
+            DoubleAnimation doubleAnimation = new DoubleAnimation(0,1, new TimeSpan(0, 0, 0, 0, 400));
+
+            Storyboard.SetTarget(thicknessAnimation, workShopDetailUC);
+            Storyboard.SetTarget(doubleAnimation, workShopDetailUC);
+
+            Storyboard.SetTargetProperty(thicknessAnimation, new PropertyPath("Margin"));
+            Storyboard.SetTargetProperty(doubleAnimation, new PropertyPath("Opacity"));
+
+            Storyboard storyboard = new Storyboard();
+            storyboard.Children.Add(thicknessAnimation);
+            storyboard.Children.Add(doubleAnimation);
+            storyboard.Begin();
+        }
+
+        /// <summary>
+        /// 返回到监控
+        /// </summary>
+        private void GoBackMonitor()
+        {
+            MonitorUC monitorUC = new MonitorUC();
+
+            mainWindowVM.MonitorUC = monitorUC;
+        }
+
+        /// <summary>
+        /// 展示详情命令
+        /// </summary>
+        public Command ShowDetailCmm
+        {
+            get
+            {
+                return new Command(ShowDetailUC);
+            }
+        }
+
+        /// <summary>
+        /// 返回监控界面命令
+        /// </summary>
+        public Command GoBackCmm
+        {
+            get
+            {
+                return new Command(GoBackMonitor);
+            }
         }
     }
 }
---
+++ ProductMonitor/ProductMonitor/OpCommand/Command.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace ProductMonitor.OpCommand
+{
+    public class Command : ICommand
+    {
+        /// <summary>
+        /// 定义委托
+        /// </summary>
+        private Action _action;
+
+        /// <summary>
+        /// 构造函数 接收方法
+        /// </summary>
+        /// <param name="action"></param>
+        public Command(Action action)
+        { 
+            this._action = action;
+        }
+
+        public event EventHandler? CanExecuteChanged;
+
+        /// <summary>
+        /// 是否可以执行
+        /// </summary>
+        /// <param name="parameter"></param>
+        /// <returns></returns>
+        public bool CanExecute(object? parameter)
+        {
+            return true;
+        }
+
+        /// <summary>
+        /// 执行方法
+        /// </summary>
+        /// <param name="parameter"></param>
+        public void Execute(object? parameter)
+        {
+            if (_action != null)
+            {
+                _action();
+            }
+        }
+    }
+}
